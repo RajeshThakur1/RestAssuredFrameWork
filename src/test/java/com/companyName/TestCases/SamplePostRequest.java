c8bc2de14780a718640bea9d59109064fb45a83c
RajeshThakur1 <rajesh.thakur@niki.ai>
2020-05-07 20:14:27 +0530

    Changing

--- src/test/java/com/companyName/TestCases/SamplePostRequest.java
+++ src/test/java/com/companyName/TestCases/SamplePostRequest.java
@@ -1,7 +1,9 @@
 package com.companyName.TestCases;
 
-import org.testng.Assert;
+import java.util.HashMap;
+
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
@@ -9,21 +11,23 @@ import com.companyName.BasePack.TestBase;
 
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
-import io.restassured.path.json.exception.JsonPathException;
-import io.restassured.response.Response;
 
-public class SampleGet extends TestBase{
-	
-	@BeforeClass
-	
-	public void sampleGetCall() {
+public class SamplePostRequest extends TestBase{
+
+	@BeforeTest
+	public void samplePostCall() {
 		InitilizeFrameWork();
 		RestAssured.baseURI= baseURI;    // As the BaseURI is the Static one so we can call directlly with help of class
-		RestAssured.basePath="/api/users?page=2";
+		RestAssured.basePath="api/users";
+		HashMap map = new HashMap();
+		map.put("name", "Rajesh");
+		map.put("job", "leader");
 		
-		response = RestAssured.given().when().get().then().extract().response();
+		response = RestAssured.given().body(map).header("Content-Type", "application/json").when().post().then().extract().response();
+		
+		
 	}
-	
+
 	
 	@Test(priority = 1)
 	public void verifyStatusCode() {
@@ -31,7 +35,7 @@ public class SampleGet extends TestBase{
 		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   Checking the status code  @@@@@@@@@@@@@@@@@@@@@");
 		log.info("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   Checking the status code  @@@@@@@@@@@@@@@@@@@@@");
 		int statusCode=response.getStatusCode();
-		soft.assertEquals(statusCode, 200);
+		soft.assertEquals(statusCode, 201);
 		soft.assertAll();
 	}
 	
@@ -44,12 +48,14 @@ public class SampleGet extends TestBase{
 		String response_Body = response.getBody().asString();
 		System.out.println("Response Body:-"+response_Body);
 		JsonPath jsonPathEvaluator = response.jsonPath();
-		int page_value=jsonPathEvaluator.get("page");
-		
+		String name = jsonPathEvaluator.get("name");
+		String job = jsonPathEvaluator.get("job");
 		// If in case isf you want to validate the more pameters u can do 
 		
-		soft.assertEquals(page_value, 1);
+		soft.assertEquals(name, "Rajesh");
+		soft.assertEquals(job, "leader");
 		
+		log.info("User has created Successfully");
 		soft.assertAll();
 		
 		
@@ -65,12 +71,8 @@ public class SampleGet extends TestBase{
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
 
+	
+	
+	
 }
